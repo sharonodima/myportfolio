e59e675535a37cf634b896fb45dc6bb2b6499b1c
Sharon Odima <odimasharon@gmail.com>
2025-01-31 14:25:43 -0500

    Final fixes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Header from './components/Header'; // Import the shared Header
-import './components/Theme.css';
+import "/src/Theme.css"
 
 function App() {
   return (
